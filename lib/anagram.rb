c4262bb4480137cd9a8058bd2a9e8e170a088f28
Joseph Lozano <lozanojoseph@gmail.com>
2015-07-03 14:02:55 -0700

    start anagram.rb

---
+++ lib/anagram.rb
@@ -0,0 +1,14 @@
+#load and split the dictionary into an array of words
+
+
+dict = []
+  
+class 
+
+
+  def anagram
+
+    return dict
+
+
+  end
---
+++ spec/anagram_spec.rb
@@ -0,0 +1,31 @@
+
+
+=begin
+  
+
+  load a dictionary
+
+  make sure input is a string
+
+
+  make sure out put is an array or nil
+
+
+  make sure out put is anagram
+
+  
+=end
+
+require 'anagram'
+
+
+  describe '#anagram' do 
+
+   it 'should load an array' do
+    expect(anagram).to be_a(Array)
+   end
+
+
+    
+  end
+
